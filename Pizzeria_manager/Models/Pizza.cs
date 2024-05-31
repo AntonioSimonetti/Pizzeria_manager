fcae12ea2f2df542ed65495c349239442a7e74d5
Antonio Simonetti <antoniosimonettiantonio@gmail.com>
2024-05-31 18:10:13 +0200

    Created class Pizza, added some media and hardcoded into the index view for test

---
+++ Pizzeria_manager/Models/Pizza.cs
@@ -0,0 +1,21 @@
+﻿namespace Pizzeria_manager.Models
+{
+    public class Pizza
+    {
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string FotoUrl { get; set; }
+        public float Prezzo { get; set; }
+
+        public Pizza() { }
+
+        public Pizza(string nome, string descrizione, string fotoUrl, float prezzo)
+        {
+            this.Nome = nome;
+            this.Descrizione = descrizione;
+            this.FotoUrl = fotoUrl;
+            this.Prezzo = prezzo;
+        }
+    }
+}
+
